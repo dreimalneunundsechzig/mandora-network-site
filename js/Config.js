4e39d50b245b8117cb38b89432e6f0e29f0fdee0
dreimalneunundsechzig <73199606+dreimalneunundsechzig@users.noreply.github.com>
2025-10-19 12:28:12 +0200

    Add files via upload

---
+++ js/Config.js
@@ -0,0 +1,39 @@
+const teamData = [
+  {
+    rank: "Projektleitung",
+    members: [
+      { name: "Leon", icon: "img/team/leon.png" },
+      { name: "Erik", icon: "img/team/erik.png" },
+      { name: "Max", icon: "img/team/max.png" }
+    ]
+  },
+  {
+    rank: "Stv. Projektleitungen",
+    members: [
+      { name: "696969", icon: "img/team/696969.png" },
+      { name: "Julian", icon: "img/team/julian.png" },
+      { name: "Legit2k", icon: "img/team/legit2k.png" }
+    ]
+  },
+  {
+    rank: "Entwicklungsleitungen",
+    members: [
+      { name: "FireNinja", icon: "img/team/fireninja.png" },
+      { name: "696969", icon: "img/team/696969.png" }
+    ]
+  },
+  {
+    rank: "Entwickler",
+    members: [
+      { name: "Evelina", icon: "img/team/evelina.png" },
+      { name: "696969", icon: "img/team/696969.png" },
+      { name: "FireNinja", icon: "img/team/fireninja.png" }
+    ]
+  },
+  {
+    rank: "Moderatoren",
+    members: [
+      { name: "Marie", icon: "img/team/marie.png" }
+    ]
+  }
+];
